4690d774597406a68701cbb7c9c6fde477d1f55f
huyhuynh1711 <huyhuynhzz256@gmail.com>
2024-04-10 17:17:18 +0700

    Updated readme for GitHub Branches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentTestComponent } from './component-test/component-test.component';
-import { ComponentOvervierByManually } from '../component-overview-by-manually/compoment-overview-by-manually';
+import { ComponentOvervierByManually } from './component-overview-by-manually/compoment-overview-by-manually';
 import { ComponentHomeComponent } from './component-home/component-home.component';
+import { FormsModule } from '@angular/forms';
+import { HelloComponent } from './hello.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentTestComponent,
     ComponentOvervierByManually,
-    ComponentHomeComponent
+    ComponentHomeComponent,
+    HelloComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+
+@Component({
+  selector: 'progress-bar',
+  template:
+  `
+        <div
+          class="progress-bar-container"
+           [style.backgroundColor]="backgroundColor"
+           >
+          <div
+           class= "progress"
+            [style]="{
+              backgroundColor: progressColor,
+               width: progress + '%'
+              }"
+          ></div>
+        </div>
+  `,
+  styles: [
+    `
+        .progress-bar-container
+        .progress{
+          height:20px;
+        }
+
+        .progress-bar-container{
+          width: 100%;
+        }
+  `
+
+  ]
+})
+export class ProgressBarComponent implements OnInit,OnChanges {
+  @Input() progress = 50;
+  @Input() backgroundColor = '#ccc';
+  @Input() progressColor = 'tomato';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log({
+      progress : this.progress,
+      backgroundColor : this.backgroundColor,
+      progressColor : this.progressColor
+    });
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log( "Changes", {
+      progress : this.progress,
+      backgroundColor : this.backgroundColor,
+      progressColor : this.progressColor
+    });
+  }
+
+}
